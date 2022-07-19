216e22c4b4b000ff6ba33cc162ddca1183b6a9d8
rBroneak <bobbydarko13@gmail.com>
2022-07-19 13:31:39 -0500

    Product in page

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,14 @@
 		button.classList.toggle("showing");
 	}
 
+	function toggleProductSearch() {
+		const element = document.getElementById("locator-content");
+		console.log(element);
+		element.classList.toggle("hidden");
+	}
+
 	document.getElementById("jsSeeAllResults").addEventListener("click", showListings);
+	document.getElementById("toggleProductSearch").addEventListener("click", toggleProductSearch);
 })();
 
 }());
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,6 +7,14 @@
 		button.classList.toggle("showing");
 	}
 
+	function toggleProductSearch() {
+		const button = this;
+		const element = document.getElementById("locator-content");
+		console.log(element)
+		element.classList.toggle("hidden");
+	}
+
 	document.getElementById("jsSeeAllResults").addEventListener("click", showListings);
+	document.getElementById("toggleProductSearch").addEventListener("click", toggleProductSearch);
 
 }());

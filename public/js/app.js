066c3da6740fc79152dd5f371e3fe6f35f736c0a
rBroneak <bobbydarko13@gmail.com>
2022-07-19 23:30:29 -0500

    Product in page

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 'use strict';
 
 (function () {
+	// Online Retailers Toggle
 	function showListings() {
 		const button = this;
 		const element = document.getElementById("jsAllResults");
@@ -10,14 +11,67 @@
 		button.classList.toggle("showing");
 	}
 
+	// Product Search Form Toggle
 	function toggleProductSearch() {
 		const element = document.getElementById("locator-content");
-		console.log(element);
 		element.classList.toggle("hidden");
 	}
 
-	document.getElementById("jsSeeAllResults").addEventListener("click", showListings);
-	document.getElementById("toggleProductSearch").addEventListener("click", toggleProductSearch);
+	const seeAllResutls = document.getElementById("jsSeeAllResults");
+	const productSearch = document.getElementById("jsToggleProductSearch");
+
+	if (seeAllResutls) {
+		seeAllResutls.addEventListener("click", showListings);
+	}
+	if (productSearch) {
+		productSearch.addEventListener("click", toggleProductSearch);
+	}
+
+	// Distributor and Retail Location Results
+	const resultToggle = $(".jsShowMoreCollection");
+	const showMoreResults = $(".jsShowMore");
+
+	function toggleNested(btn) {
+		const $button = $(btn);
+		const $collection = $(btn).closest('.collection');
+		const $hidden = $collection.children('.nested:not(:visible)').length;
+		const $footer = $collection.find('footer');
+
+		if ($collection.hasClass('showing')) {
+			$collection.removeClass('showing');
+			$button.removeClass('showing');
+			$footer.removeClass('hidden');
+			$collection.find('.nested').removeClass('d-block');
+		} else {
+			$collection.addClass('showing');
+			$button.addClass('showing');
+			if ($hidden <= 5) {
+				$footer.addClass('hidden');
+			}
+		}
+	}
+
+	$(resultToggle).click(function (event) {
+		event.preventDefault();
+		toggleNested(this);
+	});
+
+	function showMore(btn) {
+		const $button = $(btn);
+		const $collection = $(btn).closest('.collection');
+		const $hidden = $collection.children('.nested:not(:visible)').length;
+
+		if (!$hidden || $hidden <= 5) {
+			$button.parent().addClass('hidden');
+		}
+
+		$collection.children('.nested:not(:visible:lt(5))').addClass('d-block');
+	}
+
+	$(showMoreResults).click(function (event) {
+		event.preventDefault();
+		showMore(this);
+	});
 })();
 
 }());

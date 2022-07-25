ef4e4398056bcacaf9977e5280051599b897fd5c
rBroneak <bobbydarko13@gmail.com>
2022-07-24 22:43:18 -0500

    Comments and Sales expert block in place.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,73 +1,74 @@
-(function () {
-	// Online Retailers Toggle
-	function showListings() {
-		const button = this;
-		const element = document.getElementById("jsAllResults");
+(function() {
+  // Online Retailers Toggle
+  function showListings() {
+    const button = this;
+    const element = document.getElementById("jsAllResults");
 
-		element.classList.toggle("collapsed");
-		button.classList.toggle("showing");
-	}
+    element.classList.toggle("collapsed");
+    button.classList.toggle("showing");
+  }
 
-	// Product Search Form Toggle
-	function toggleProductSearch() {
-		const element = document.getElementById("locator-content");
-		element.classList.toggle("hidden");
-	}
+  // Product Search Form Toggle
+  function toggleProductSearch() {
+    const contentToToggle = $(this).data("target");
+    const element = document.getElementById(contentToToggle);
+    element.classList.toggle("hidden");
+  }
 
-	const seeAllResutls = document.getElementById("jsSeeAllResults");
-	const productSearch = document.getElementById("jsToggleProductSearch");
+  const seeAllResutls = document.getElementById("jsSeeAllResults");
+  const productSearch = document.getElementById("jsToggleProductSearch");
 
-	if (seeAllResutls) {
-		seeAllResutls.addEventListener("click", showListings);
-	}
-	if (productSearch) {
-		productSearch.addEventListener("click", toggleProductSearch);
-	}
+  if (seeAllResutls) {
+    seeAllResutls.addEventListener("click", showListings);
+  }
+  if (productSearch) {
+    productSearch.addEventListener("click", toggleProductSearch);
+  }
 
-	// Distributor and Retail Location Results
-	const resultToggle = $(".jsShowMoreCollection");
-	const showMoreResults = $(".jsShowMore")
+  // Distributor and Retail Location Results
+  const resultToggle = $(".jsShowMoreCollection");
+  const showMoreResults = $(".jsShowMore");
 
-	function toggleNested(btn) {
-		const $button = $(btn);
-		const $collection = $(btn).closest('.collection');
-		const $hidden = $collection.children('.nested:not(:visible)').length;
-		const $footer = $collection.find('footer');
+  function toggleNested(btn) {
+    const $button = $(btn);
+    const $collection = $(btn).closest(".collection");
+    const $hidden = $collection.children(".nested:not(:visible)").length;
+    const $footer = $collection.find("footer");
 
-		if ($collection.hasClass('showing')) {
-			$collection.removeClass('showing');
-			$button.removeClass('showing');
-			$footer.removeClass('hidden')
-			$collection.find('.nested').removeClass('d-block')
-		} else {
-			$collection.addClass('showing');
-			$button.addClass('showing');
-			if ($hidden <= 5 ) {
-				$footer.addClass('hidden')
-			}
-		}
-	}
+    if ($collection.hasClass("showing")) {
+      $collection.removeClass("showing");
+      $button.removeClass("showing");
+      $footer.removeClass("hidden");
+      $collection.find(".nested").removeClass("d-block");
+    } else {
+      $collection.addClass("showing");
+      $button.addClass("showing");
+      if ($hidden <= 5) {
+        $footer.addClass("hidden");
+      }
+    }
+  }
 
-	$(resultToggle).click(function (event){
-		event.preventDefault()
-		toggleNested(this)
-	});
+  $(resultToggle).click(function(event) {
+    event.preventDefault();
+    toggleNested(this);
+  });
 
-	function showMore(btn) {
-		const $button = $(btn);
-		const $collection = $(btn).closest('.collection');
-		const $hidden = $collection.children('.nested:not(:visible)').length;
+  function showMore(btn) {
+    const $button = $(btn);
+    const $collection = $(btn).closest(".collection");
+    const $hidden = $collection.children(".nested:not(:visible)").length;
 
-		if (!$hidden || $hidden <= 5 ) {
-			$button.parent().addClass('hidden')
-		}
+    if (!$hidden || $hidden <= 5) {
+      $button.parent().addClass("hidden");
+    }
 
-		$collection.children('.nested:not(:visible:lt(5))').addClass('d-block');
-	}
+    $collection.children(".nested:not(:visible:lt(5))").addClass("d-block");
+  }
 
-	$(showMoreResults).click(function (event){
-		event.preventDefault();
-		showMore(this);
-	})
+  $(showMoreResults).click(function(event) {
+    event.preventDefault();
+    showMore(this);
+  });
 
 }());
